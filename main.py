efb8febabd69cbe2e2a11dd8927065c186deed6b
bappine <baphongpine@gmail.com>
2026-01-05 22:50:32 +0700

    init: todo app

---
+++ main.py
@@ -0,0 +1,31 @@
+# TASK TODO CRUD BASIC
+# C: create R: read U: update D: delete
+
+
+# 1. Tạo một mảng để lưu các todo
+todos = []
+
+
+# 2. Tạo hàm để đọc toàn bộ các todo đó
+
+
+
+
+# 3. Tạo hàm để thêm một todo mới vào mảng trêns
+
+
+
+
+# 4. Tạo hàm để xóa một todo dựa trên chỉ số của nó trong mảng
+
+
+
+# 5. Tạo hàm để cập nhật một todo dựa trên chỉ số của nó trong mảng
+
+
+# 6. Tạo hàm xóa toàn bộ các todo trong mảng
+
+
+
+
+
