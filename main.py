22afdf39850952cedee17e1cb8d347e77e75c9da
Kunn3577 <phamhuuhongan3577@gmail.com>
2026-01-05 23:35:08 +0700

    feat: Tạo hàm show todo_list

--- main.py
+++ main.py
@@ -7,28 +7,19 @@ todos = []
 
 
 # 2. Tạo hàm để đọc toàn bộ các todo đó (Ân)
-
-
+def read(todos):
+    print("------Danh sách công việc------")
+    for i in range(len(todos)):
+        print(f"{i+1}: {todos[i]}")
 
 
 # 3. Tạo hàm để thêm một todo mới vào mảng trên (Huy)
 
 
-
-
 # 4. Tạo hàm để xóa một todo dựa trên chỉ số của nó trong mảng (Quyền)
 
 
-
 # 5. Tạo hàm để cập nhật một todo dựa trên chỉ số của nó trong mảng (Nhân)
-def update_todos(index, new_task):
-    if 0 <= index < len(todos):
-        todos[index] = new_task
-        print(f"Đã cập nhật vị trí số {index} thành: '{new_task}'")
-        return True
-    else:
-        print(f"Lỗi: Chỉ số {index} không tồn tại trong danh sách.")
-        return False
 
 
 # 6. Tạo hàm xóa toàn bộ các todo trong mảng (Bùi)
@@ -37,8 +28,3 @@ def update_todos(index, new_task):
 # 7. Xóa toàn bộ các todo trong mảng
 def clear_todos():
     todos.clear()
-
-
-
-
-
