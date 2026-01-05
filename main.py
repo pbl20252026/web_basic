7f1254261b92d187de6e9067be65fb1650654eba
bappine <baphongpine@gmail.com>
2026-01-05 22:58:23 +0700

    feat: xóa toàn bộ các todo trong mảng

--- main.py
+++ main.py
@@ -6,24 +6,29 @@
 todos = []
 
 
-# 2. Tạo hàm để đọc toàn bộ các todo đó
+# 2. Tạo hàm để đọc toàn bộ các todo đó (Ân)
 
 
 
 
-# 3. Tạo hàm để thêm một todo mới vào mảng trêns
+# 3. Tạo hàm để thêm một todo mới vào mảng trên (Huy)
 
 
 
 
-# 4. Tạo hàm để xóa một todo dựa trên chỉ số của nó trong mảng
+# 4. Tạo hàm để xóa một todo dựa trên chỉ số của nó trong mảng (Quyền)
 
 
 
-# 5. Tạo hàm để cập nhật một todo dựa trên chỉ số của nó trong mảng
+# 5. Tạo hàm để cập nhật một todo dựa trên chỉ số của nó trong mảng (Nhân)
 
 
-# 6. Tạo hàm xóa toàn bộ các todo trong mảng
+# 6. Tạo hàm xóa toàn bộ các todo trong mảng (Bùi)
+
+
+# 7. Xóa toàn bộ các todo trong mảng
+def clear_todos():
+    todos.clear()
 
 
 
